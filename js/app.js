f4160263cfa8225e05661c16b1cbea5a111eeebb
gavinbrown025 <you@example.com>
2020-01-31 10:32:52 -0500

    picture functions

--- js/app.js
+++ js/app.js
@@ -2,14 +2,21 @@
 	// set up the puzzle pieces and boards
 	// navButtons -> images at the bottom of the page
 	const navButtons = document.querySelectorAll('#buttonHolder img'),
-		puzzlePieces = document.querySelectorAll('.puzzle-images');
+		puzzlePiece = document.querySelectorAll('.puzzle-pieces img'),
+		puzzleBoard = document.querySelector('.puzzle-board');
+
+	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
 	// functions go here => what we want to have happen when our triggers fire
 	function changeImageSet() {
 		// change the thumbnail images on the left to match the button images
+		pieces.forEach((piece, index) => puzzlePiece[index].src=`images/${piece + this.dataset.puzzleindex}.jpg`);
+		
 		// and set a background image on the drop zone container
+		
 		puzzleBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleindex}.jpg`;
-		puzzlePieces.src = `url(images/topRight${this.dataset.puzzlepieces}.jpg`;
+
+
 		//debugger;
 }
 
